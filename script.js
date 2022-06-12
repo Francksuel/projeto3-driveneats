0f4508ab1c21c3094553528afb37977b0866895f
Francksuel <francksuelbarbosa@gmail.com>
2022-06-12 00:29:41 -0300

    Projeto Finalizado(sem bonus)

--- script.js
+++ script.js
@@ -55,12 +55,22 @@ function selSobremesa(selecionado) {
       fechar();
    }
 }
+
 function fechar() {
    if (document.querySelector(".comidas .select") !== null && document.querySelector(".bebidas .select") !== null && document.querySelector(".sobremesas .select") !== null) {
-      document.querySelector("button").innerHTML = "Fechar o pedido";
-      document.querySelector("button").classList.add("completed")
+      let item1=document.querySelector(".comidas .select h4").innerHTML;
+      let item2=document.querySelector(".bebidas .select h4").innerHTML;
+      let item3=document.querySelector(".sobremesas .select h4").innerHTML;
+      let valor1=document.querySelector(".comidas .select h6").innerHTML.slice(3,7).replace(",",".")
+      let valor2=document.querySelector(".bebidas .select h6").innerHTML.slice(3,7).replace(",",".")
+      let valor3=document.querySelector(".sobremesas .select h6").innerHTML.slice(3,7).replace(",",".")     
+   let total=Number(valor1)+Number(valor2)+Number(valor3)
+      let mensagem= (`Olá, gostaria de fazer o pedido:\n- Prato: ${item1}\n- Bebida: ${item2}\n- Sobremesa: ${item3}\nTotal: R$ ${total.toFixed(2)}`);
+      mensagem=encodeURIComponent(mensagem)
+      document.querySelector(".menuinf").innerHTML = `<a href="https://wa.me/5584998551416?text=${mensagem}" target="blank"><button>Fechar o pedido</button></a>`
+      document.querySelector("button").classList.add("completed")      
    } else {
-      document.querySelector("button").innerHTML = "Selecione os 3 itens para fechar o pedido";
+      document.querySelector(".menuinf").innerHTML = "<button>Selecione os 3 itens para fechar o pedido</button>"
       document.querySelector("button").classList.remove("completed")
    }
 }
