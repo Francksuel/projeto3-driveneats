3b572f9a871f4feea0c9f0cf0bca087f62e91ae9
Francksuel <francksuelbarbosa@gmail.com>
2022-06-13 11:53:09 -0300

    Projeto finalizado (1º bonûs)

--- script.js
+++ script.js
@@ -58,19 +58,25 @@ function selSobremesa(selecionado) {
 
 function fechar() {
    if (document.querySelector(".comidas .select") !== null && document.querySelector(".bebidas .select") !== null && document.querySelector(".sobremesas .select") !== null) {
-      let item1=document.querySelector(".comidas .select h4").innerHTML;
-      let item2=document.querySelector(".bebidas .select h4").innerHTML;
-      let item3=document.querySelector(".sobremesas .select h4").innerHTML;
-      let valor1=document.querySelector(".comidas .select h6").innerHTML.slice(3,7).replace(",",".")
-      let valor2=document.querySelector(".bebidas .select h6").innerHTML.slice(3,7).replace(",",".")
-      let valor3=document.querySelector(".sobremesas .select h6").innerHTML.slice(3,7).replace(",",".")     
-   let total=Number(valor1)+Number(valor2)+Number(valor3)
-      let mensagem= (`Olá, gostaria de fazer o pedido:\n- Prato: ${item1}\n- Bebida: ${item2}\n- Sobremesa: ${item3}\nTotal: R$ ${total.toFixed(2)}`);
-      mensagem=encodeURIComponent(mensagem)
-      document.querySelector(".menuinf").innerHTML = `<a href="https://wa.me/5584998551416?text=${mensagem}" target="blank"><button>Fechar o pedido</button></a>`
-      document.querySelector("button").classList.add("completed")      
+
+      document.querySelector(".menuinf").innerHTML = `<button onclick="send()">Fechar o pedido</button>`
+      document.querySelector("button").classList.add("completed")
    } else {
       document.querySelector(".menuinf").innerHTML = "<button>Selecione os 3 itens para fechar o pedido</button>"
       document.querySelector("button").classList.remove("completed")
    }
+}
+function send (){
+let item1 = document.querySelector(".comidas .select h4").innerHTML;
+let item2 = document.querySelector(".bebidas .select h4").innerHTML;
+let item3 = document.querySelector(".sobremesas .select h4").innerHTML;
+let valor1 = document.querySelector(".comidas .select h6").innerHTML.slice(3, 7).replace(",", ".")
+let valor2 = document.querySelector(".bebidas .select h6").innerHTML.slice(3, 7).replace(",", ".")
+let valor3 = document.querySelector(".sobremesas .select h6").innerHTML.slice(3, 7).replace(",", ".")
+let total = Number(valor1) + Number(valor2) + Number(valor3)
+let nome = prompt("Qual seu nome?")
+let endereco = prompt("Qual endereço de entrega?")
+let mensagem = (`Olá, gostaria de fazer o pedido:\n- Prato: ${item1}\n- Bebida: ${item2}\n- Sobremesa: ${item3}\nTotal: R$ ${total.toFixed(2)}\n\nNome: ${nome}\nEndereço: ${endereco}`);
+mensagem = encodeURIComponent(mensagem)
+window.open(`https://wa.me/5584998551416?text=${mensagem}`,'_blank')
 }
